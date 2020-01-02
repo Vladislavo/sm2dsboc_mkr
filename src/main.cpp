8d0475b946dfa57aec32d9ceb71b0fd4931e4d54
vladislav <vladislavrykov@hotmail.com>
2020-01-02 14:32:05 +0100

    adc added dev

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <Arduino.h>
-#include "ADS1115.h"
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+#define BUS_PROTOCOL_TRANSMIT_RETRIES       5
+#define BUS_PROTOCOL_MAX_DATA_SIZE          32
+#define DEBUG_SERIAL_RX_PIN                 2
+#define DEBUG_SERIAL_TX_PIN                 3
+
+#define BAUDRATE                            115200
+#define DHT22_PIN                           A6
+
+#define DATA_SEND_PERIOD                    2000
+
+Adafruit_ADS1115 ads;
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(BAUDRATE);
+    Wire.begin();
+
+    ads.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    int16_t adc0, adc1, adc2;
+
+    adc0 = ads.readADC_SingleEnded(0);
+    adc1 = ads.readADC_SingleEnded(1);
+    adc2 = ads.readADC_SingleEnded(2);
+
+    delay(1000);
 }
